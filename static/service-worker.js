d3bfc7faf1cbbb38ea9c05558030d5016c918fbc
denmraq <bvg227fwk4@privaterelay.appleid.com>
2026-08-27 19:15:53 +0200

    Delete static/service-worker.js

--- static/service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE='eth-radar-core-v050-scalp';
-const ASSETS=['/','/manifest.webmanifest','/static/icon-192.png','/static/icon-512.png','/static/apple-touch-icon.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.pathname.startsWith('/api/')){e.respondWith(fetch(e.request,{cache:'no-store'}));return;}
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
